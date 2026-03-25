1b241712f76b8ab07eed043c74672c8f2e7baf49
KyungWoon Cho <cezanne@codemayo.com>
2026-03-25 16:12:08 +0900

    Remove MPI dependency
    Remove the MPI dependency from strace-wrapper.sh.
    This tool is intended to be run on a single node rather than in an MPI
    environment.

--- strace-wrapper.sh
+++ strace-wrapper.sh
@@ -1,25 +1,22 @@
 #!/bin/sh
-#
-# mpirun [MPI OPTIONS] ./strace-mpi-wrapper.sh APP [APP OPTIONS]
-#
 
-cd $PWD
+# Ensure the current directory is accessible and synchronized
+cd "$PWD" || exit 1
 
-MPI_RANK=${MPI_RANK:=$PMI_RANK}
-MPI_RANK=${MPI_RANK:=$PMIX_RANK}
-MPI_RANK=${MPI_RANK:=$OMPI_COMM_WORLD_RANK}
-MPI_RANK=${MPI_RANK:=$ALPS_APP_PE}
-
+# Set the log directory (default: strace-logs)
 LOGDIR=${LOGDIR:=strace-logs}
-if ! test -d $LOGDIR ; then
-  mkdir -p $LOGDIR
-fi
-LOGFILE="$LOGDIR/${MPI_RANK:+r${MPI_RANK}-}$(hostname).$$.strace"
 
-
-if  ! test -z $MPI_RANK  &&  ! test -z $RANK_FILTER  &&  ! [[ $MPI_RANK =~ $RANK_FILTER ]] ; then
-  echo "# Rank $MPI_RANK not traced" > $LOGFILE
-  "$@"
-else
-  strace -f -r -T -o "$LOGFILE" "$@"
+# Create the log directory if it does not exist
+if [ ! -d "$LOGDIR" ]; then
+  mkdir -p "$LOGDIR"
 fi
+
+# Set the log filename with hostname and Process ID (PID)
+LOGFILE="$LOGDIR/$(hostname).$$.strace"
+
+# Execute strace with the provided arguments ($@)
+# -f: Follow child processes
+# -r: Print relative timestamp
+# -T: Print time spent in system calls
+# -o: Write the trace output to a file
+strace -f -r -T -o "$LOGFILE" "$@"
