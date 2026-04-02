db84cccd208fecf5852e67fe749e18738e5e4c6c
KyungWoon Cho <cezanne@codemayo.com>
2026-04-02 17:48:35 +0900

    Diversify reporting methods
    Refactored strace analysis result reporting to support multiple formats.
    Migrated argument parsing from optparse to argparse.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import optparse
+import argparse
 import logging
 import sys
 
@@ -7,21 +7,31 @@ from scan import scan_strace
 from report import report
 
 
-def main():
-    parser = optparse.OptionParser("usage: %prog [options] STRACE_LOG ...")
-    parser.add_option('--loglevel', default="ERROR", help="CRITICAL, ERROR, WARNING, INFO, DEBUG")
+def parse_argument():
+    parser = argparse.ArgumentParser("usage: %prog [options] STRACE_LOG ...")
+    parser.add_argument('--loglevel', type=str, default="ERROR", help="CRITICAL, ERROR, WARNING, INFO, DEBUG")
+    parser.add_argument("--path-pattern", action='append', dest='path_patterns',
+                        help="Patterns to match for reporting. Can be specified multiple times.")
+    parser.add_argument('--report-mode', type=str, default="summary",
+                        help="report mode: raw/summary/timeline")
+    parser.add_argument('strace_files', nargs='+', help="strace log files to process")
 
-    (options, args) = parser.parse_args()
+    args = parser.parse_args()
     if not args:
         parser.print_help()
         sys.exit(1)
+    return args
+
+
+def main():
+    args = parse_argument()
 
     # noinspection SpellCheckingInspection
-    logging.basicConfig(level=getattr(logging, options.loglevel.upper()), format='%(levelname)s: %(message)s')
+    logging.basicConfig(level=getattr(logging, args.loglevel.upper()), format='%(levelname)s: %(message)s')
 
     strace_parser = Parser()
-    scan_strace(strace_parser, args)
-    report(strace_parser.fio_manager)
+    scan_strace(strace_parser, args.strace_files)
+    report(args.report_mode, args.path_patterns, strace_parser.fio_manager)
 
 
 if __name__ == "__main__":
--- report.py
+++ report.py
@@ -1,9 +1,68 @@
+from typing import List
+from fnmatch import fnmatch
+
 from fio_manager import FileIoManager
 
 
-def report(fio_manager: FileIoManager):
-    print("File IO Report:")
+def match_patterns(path: str, patterns: List[str]) -> bool:
+    if patterns is None:
+        return True
+    for pattern in patterns:
+        if fnmatch(path, f"{pattern}*"):
+            return True
+    return False
+
+
+def report_summary(path_patterns: List[str], fio_manager: FileIoManager):
+    total_read = 0
+    total_write = 0
+    for fio in fio_manager.file_ios:
+        if not match_patterns(fio.path, path_patterns):
+            continue
+        if fio.is_read:
+            total_read += fio.size
+        else:
+            total_write += fio.size
+
+    print("File IO Summary:")
+    print("===================")
+    print(f"Total Read: {total_read} bytes")
+    print(f"Total Write: {total_write} bytes")
+
+
+def report_timeline(path_patterns: List[str], fio_manager: FileIoManager):
+    timeslot_read = 0
+    timeslot_write = 0
+    rtime_next = 1
+    for fio in fio_manager.file_ios:
+        if not match_patterns(fio.path, path_patterns):
+            continue
+        if fio.is_read:
+            timeslot_read += fio.size
+        else:
+            timeslot_write += fio.size
+        while rtime_next <= fio.rtime:
+            print(f"{rtime_next - 1}: Read: {timeslot_read} bytes, Write: {timeslot_write} bytes")
+            timeslot_read = 0
+            timeslot_write = 0
+            rtime_next += 1
+    print(f"{rtime_next - 1}: Read: {timeslot_read} bytes, Write: {timeslot_write} bytes")
+
+
+def report_raw(path_patterns: List[str], fio_manager: FileIoManager):
+    print("File IO Details:")
     print("===================")
     for fio in fio_manager.file_ios:
+        if not match_patterns(fio.path, path_patterns):
+            continue
         typestr = 'Read' if fio.is_read else 'Write'
         print(f"Time: {fio.rtime:.6f}s, File: {fio.path}, {typestr}: {fio.size}")
+
+
+def report(mode: str, path_patterns: List[str], fio_manager: FileIoManager):
+    if mode == "summary":
+        report_summary(path_patterns, fio_manager)
+    elif mode == "timeline":
+        report_timeline(path_patterns, fio_manager)
+    else:
+        report_raw(path_patterns, fio_manager)
